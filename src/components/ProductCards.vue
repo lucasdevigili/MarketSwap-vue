1db87260faf508ca0a4aeb9af941fb51ff5f3646
Tutu <adamann@furb.br>
2024-04-21 19:20:04 -0300

    cards

---
+++ src/components/ProductCards.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="card">
+    <div id="caixa">
+    </div>
+    <div>
+<h3>Nome do Produto</h3>
+<h3>Preço</h3>
+<h6>Categoria</h6>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'ProductCards'
+}
+</script>
+
+<style scoped>
+
+.card {
+    widows: 205px;
+    height: 287px;
+    
+    border-color: black;
+
+}
+#caixa {
+    width: 205px;
+    height: 205px;
+    background-color: gray;
+    border-radius: 12px;
+}
+
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,37 @@
 <template>
     <div class="home">
+
+
+
         <TopNav />
+        
+
         <main>
+            <ProductCards />
             <NavRail />
+
+            
+
+
         </main>
     </div>
+
+
+
+
 </template>
 
 <script>
 import TopNav from '@/components/TopNav.vue'
 import NavRail from '@/components/NavRail/NavRail.vue'
+import ProductCards from '@/components/ProductCards.vue'
 
 export default {
     name: 'HomeView',
     components: {
         TopNav,
-        NavRail
+        NavRail,
+        ProductCards
     }
 }
 
@@ -26,7 +42,10 @@ export default {
     height: 100vh;
     width: 100vw;
     background-color: #F0F4FA;
+
+
 }
+
 
 main {
     height: 85vh;
